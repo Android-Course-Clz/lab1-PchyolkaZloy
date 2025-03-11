1a3d50744dbc9adc3e22a5d034ec7b0f3805e44a
pchz <pchz@gmail.com>
2025-03-11 22:18:39 +0300

    feat: added comments

--- app/src/main/java/en/pchz/lab1/adapters/view/holders/PostViewHolder.kt
+++ app/src/main/java/en/pchz/lab1/adapters/view/holders/PostViewHolder.kt
@@ -1,6 +1,7 @@
 package en.pchz.lab1.adapters.view.holders
 
 import android.view.View
+import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
@@ -8,13 +9,20 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import en.pchz.lab1.R
 import en.pchz.lab1.dto.Post
+import androidx.core.view.isGone
 
 class PostViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val postImage: ImageView = itemView.findViewById(R.id.post_image)
     private val postText: TextView = itemView.findViewById(R.id.post_text)
+
     private val likesCount: TextView = itemView.findViewById(R.id.likes_count)
     private val likeButton: ImageButton = itemView.findViewById(R.id.like_button)
+
     private val commentCount: TextView = itemView.findViewById(R.id.comments_count)
+    private val commentButton: ImageButton = itemView.findViewById(R.id.comment_button)
+    private val commentInput: EditText = itemView.findViewById(R.id.comment_input)
+    private val sendCommentButton: ImageButton = itemView.findViewById(R.id.send_comment_button)
+
 
     private var isLiked = false
     private var currentLikes = 0
@@ -42,7 +50,24 @@ class PostViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
                 currentLikes--
                 likeButton.setImageResource(R.drawable.ic_heart_outline)
             }
-            likesCount.text = "$currentLikes likes"
+            likesCount.text = itemView.context.getString(R.string.likes_count, currentLikes)
+        }
+
+        commentButton.setOnClickListener {
+            if (commentInput.isGone) {
+                commentInput.visibility = View.VISIBLE
+                sendCommentButton.visibility = View.VISIBLE
+            } else {
+                commentInput.visibility = View.GONE
+                sendCommentButton.visibility = View.GONE
+            }
+        }
+
+        sendCommentButton.setOnClickListener {
+            val commentText = commentInput.text.toString()
+            if (commentText.isNotEmpty()) {
+                commentInput.text.clear()
+            }
         }
     }
 }
